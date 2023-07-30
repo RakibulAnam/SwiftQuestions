c0a6a9a48549474057873e3e0dc2572eb5e83bbd
RakibulAnam <rakibulanamofficial@gmail.com>
2023-07-30 14:13:53 +0600

    Solved Generics

---
+++ ExtensionAndComputedProperties/ExtensionAndComputedProperties/main.swift
@@ -0,0 +1,26 @@
+
+
+
+func swapTwoValues<T>(n1:T, n2:T){
+    
+    print("N1 is \(n1) and n2 is \(n2)")
+    
+    var c : T
+    var a = n1
+    var b = n2
+    
+    c = a
+    a = b
+    b = c
+    
+    print("After swap")
+    print("")
+    print("N1 is \(a) and n2 is \(b)")
+    
+}
+
+// With String
+swapTwoValues(n1: 10, n2: 24)
+
+//With Int
+swapTwoValues(n1: "Rohid", n2: "MOhit")
